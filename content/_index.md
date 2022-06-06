eb4351dc91413563f1257a4e9343138281c03b18
Tom Delmas <1955774+tdelmas@users.noreply.github.com>
2022-06-06 12:05:52 +0200

    Ouverture du local

--- content/_index.md
+++ content/_index.md
@@ -4,4 +4,10 @@ title: "Bienvenue sur le site de l'association *Roues Libres*"
 
 Nous avons pour objectif de promouvoir l’utilisation du vélo à Charenton-Le-Pont en proposant des ateliers d'auto-réparation de vélos.
 
+## Ouverture du local
+
+Notre local ouvre le **12 juin**, au *84 Quai des Carrières* à Charenton.
+
+[Commment y venir ?](/local/#comment-y-venir-)
+
 [Toutes les options pour réparer son vélo à Charenton](/reparer-son-velo)
---
+++ static/js/index.js
@@ -0,0 +1,35 @@
+const ua = navigator.userAgent.toLowerCase();
+
+const isAndroid = ua.indexOf("android") > -1;
+const isIos =
+  [
+    "iPad Simulator",
+    "iPhone Simulator",
+    "iPod Simulator",
+    "iPad",
+    "iPhone",
+    "iPod",
+  ].includes(navigator.platform) ||
+  (ua.includes("Mac") && "ontouchend" in document);
+
+if (isAndroid || isIos) {
+  const links = document.querySelectorAll(
+    '[href^="https://www.openstreetmap.org/"]'
+  );
+
+  links.forEach((link) => {
+    let href = link.getAttribute("href");
+
+    const hash = href.split("#")[1];
+    const [undefined, lat, lng] = hash.split("/");
+
+    let link;
+    if (isAndroid) {
+      href = `geo:${lat},${lng}?q=Roues Libres Charenton`;
+    } else if (isIos) {
+      href = `http://maps.apple.com/?ll=${lat},${lng}&address=84 Quai des Carrières&q=Roues Libres Charenton`;
+    }
+
+    link.setAttribute("href", href);
+  });
+}
